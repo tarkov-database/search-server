e661c8d173ee125a584045abd7453697aab94d37
Markus Wiegand <mail@morphy2k.dev>
2020-07-19 02:03:56 +0200

    add index state updating

--- search-rest/src/main.rs
+++ search-rest/src/main.rs
@@ -1,6 +1,12 @@
-use std::{env, io, process, sync::Arc};
+use std::{
+    env, io, process,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
+use actix::{Actor, AsyncContext, Context};
 use actix_web::{dev::HttpResponseBuilder, http::StatusCode, web, App, HttpServer, Responder};
+use chrono::{DateTime, TimeZone, Utc};
 use search_index::{tarkov_database_rs::client::Client, ItemIndex};
 use serde::{Deserialize, Serialize};
 
@@ -8,7 +14,88 @@ use serde::{Deserialize, Serialize};
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-const PORT: &str = "8080";
+const PORT: u16 = 8080;
+
+const UPDATE_INTERVAL: u64 = 15 * 60;
+
+struct IndexStateHandler {
+    client: Arc<Mutex<Client>>,
+    interval: Duration,
+    item_index: Arc<IndexState<ItemIndex>>,
+}
+
+struct IndexState<T> {
+    index: T,
+    modified: Mutex<DateTime<Utc>>,
+}
+
+impl IndexStateHandler {
+    fn update_indexes(&mut self, ctx: &mut Context<Self>) {
+        let client = self.client.clone();
+        let item_index = self.item_index.clone();
+
+        ctx.spawn(actix::fut::wrap_future(async move {
+            let mut c_client = client.lock().unwrap();
+
+            if !c_client.token_is_valid() {
+                if let Err(e) = c_client.refresh_token().await {
+                    eprintln!(
+                        "Couldn't update indexes: error while refreshing API token: {}",
+                        e
+                    );
+                    return;
+                }
+            }
+
+            let item_stats = match c_client.get_item_index().await {
+                Ok(i) => i,
+                Err(e) => {
+                    eprintln!(
+                        "Couldn't update indexes: error while getting item index: {}",
+                        e
+                    );
+                    return;
+                }
+            };
+
+            let mut c_modified = item_index.modified.lock().unwrap();
+
+            if c_modified.ge(&item_stats.modified) {
+                return;
+            }
+
+            let items = match c_client.get_all_items().await {
+                Ok(d) => d,
+                Err(e) => {
+                    eprintln!(
+                        "Couldn't update indexes: error while getting items from API: {}",
+                        e
+                    );
+                    return;
+                }
+            };
+
+            if let Err(e) = item_index.index.write_index(items) {
+                eprintln!(
+                    "Couldn't update indexes: error while writing item index: {}",
+                    e
+                );
+                return;
+            }
+
+            *c_modified = item_stats.modified;
+        }));
+    }
+}
+
+impl Actor for IndexStateHandler {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Context<Self>) {
+        self.update_indexes(ctx);
+        ctx.run_interval(self.interval, Self::update_indexes);
+    }
+}
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -31,10 +118,10 @@ struct Query {
 }
 
 async fn item_query_handler(
-    index: web::Data<Arc<ItemIndex>>,
+    state: web::Data<Arc<IndexState<ItemIndex>>>,
     query: web::Query<Query>,
 ) -> impl Responder {
-    match index.query_top(&query.term, query.limit) {
+    match state.index.query_top(&query.term, query.limit) {
         Ok(d) => HttpResponseBuilder::new(StatusCode::OK).json(Response {
             count: d.len(),
             data: d,
@@ -66,47 +153,44 @@ async fn main() -> io::Result<()> {
         }
     };
 
-    env::set_var("RUST_LOG", "info");
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
     env_logger::init();
 
-    let mut api_client = Client::with_host(&api_token, &api_host);
-
-    if !api_client.token_is_valid() {
-        if let Err(e) = api_client.refresh_token().await {
-            eprintln!("Error while refreshing API token: {}", e);
-            process::exit(2);
-        }
-    }
-
-    let items = match api_client.get_all_items().await {
-        Ok(d) => d,
-        Err(e) => {
-            eprintln!("Error while getting items from API: {}", e);
-            process::exit(2);
-        }
-    };
-
-    let item_index = Arc::new(match ItemIndex::new() {
-        Ok(i) => i,
-        Err(e) => {
-            eprintln!("Error while creating item index: {}", e);
-            process::exit(2);
-        }
+    let item_index = Arc::new(IndexState {
+        index: match ItemIndex::new() {
+            Ok(i) => i,
+            Err(e) => {
+                eprintln!("Error while creating item index: {}", e);
+                process::exit(2);
+            }
+        },
+        modified: Mutex::new(Utc.timestamp(0, 0)),
     });
-    if let Err(e) = item_index.write_index(items) {
-        eprintln!("Error while writing item index: {}", e);
-        process::exit(2);
-    }
 
-    let factory = move || {
+    let update_interval = Duration::from_secs(
+        env::var("UPDATE_INTERVAL")
+            .unwrap_or_default()
+            .parse()
+            .unwrap_or(UPDATE_INTERVAL),
+    );
+
+    IndexStateHandler::create(|_ctx| IndexStateHandler {
+        client: Arc::new(Mutex::new(Client::with_host(&api_token, &api_host))),
+        interval: update_interval,
+        item_index: item_index.clone(),
+    });
+
+    let server = HttpServer::new(move || {
         App::new().service(
             web::resource("/item")
                 .data(item_index.clone())
                 .to(item_query_handler),
         )
-    };
+    })
+    .bind(bind)?
+    .run();
 
-    let server = HttpServer::new(factory).bind(bind)?;
-
-    server.run().await
+    server.await
 }
